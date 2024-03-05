2edfae1ccb4b83366a9fe885ca176c7ffa76763c
Ojo damilola <dammy3114@gmail.com>
2024-03-05 01:14:03 +0100

    project initialisation

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='test'>
+        Hello drug website
+        <button>hello</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
